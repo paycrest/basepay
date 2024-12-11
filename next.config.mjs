186f6f50809abcf5312f16c353bf5734eb7d9794
Paycrest Admin <128632949+paycrest-dev@users.noreply.github.com>
2024-12-11 02:21:13 +0000

    fix: added webpack config in nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      async_hooks: false, 
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
